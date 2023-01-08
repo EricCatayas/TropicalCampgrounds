4cbbfed2eb701e64049afb6f55bad00b984eb9f2
MaggotScientist <catayasericjay@gmail.com>
2023-01-08 16:24:08 +0800

    Added a test script simply

---
+++ example.test.js
@@ -0,0 +1,6 @@
+// The purpose of a test script is to make it easier to run your tests. Instead of having to remember and type out the command to run your tests every time, you can simply type npm test in your terminal to run all of your tests.
+// Test scripts are particularly useful when you have a large codebase with many different tests
+
+test('example test', () => {
+    expect(true).toBe(true);
+  });
